1469c22bff0de7c0abd08e9d47ece7b0843cc7d6
Hadrien Perier <hadrien.perier@ynov.com>
2023-01-23 19:41:12 +0100

    Project Base Push
    Base of the project to start working as a team

---
+++ RayziaxCorpProject/app/src/main/java/io/ynov/rayziaxcorpproject/MainActivity.kt
@@ -0,0 +1,17 @@
+package io.ynov.rayziaxcorpproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(MainActivity::class.simpleName, "CURRENTLY IN onStart()")
+    }
+}
